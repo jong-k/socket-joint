030b57a85b0ad79220bb8284a2f17b784192fcf9
jong-k <fully.charged07@gmail.com>
2023-08-06 23:45:35 +0900

    ===== Web Socket 프로젝트 피보팅 =====

--- src/app/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { commentsReducer } from "../features/Comments/commentsSlice";
-
-export const store = configureStore({
-  reducer: {
-    comments: commentsReducer,
-  },
-});
