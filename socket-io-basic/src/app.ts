1239e9c11e761081e53c92b458d86f487e7b6ed5
jhkim-hdxwill <jhkim00@iwillmed.com>
2023-08-08 11:07:22 +0900

    chore: 주석 추가

--- socket-io-basic/src/app.ts
+++ socket-io-basic/src/app.ts
@@ -8,7 +8,7 @@ const expressServer = app.listen(3000);
 const io = new Server(expressServer, {});
 
 io.on("connection", (socket) => {
-  // id 는 서버가 실행될 때마다 랜덤 문자열로 지정됨
+  // id 는 서버가 실행될 때마다 20자의 랜덤 문자열로 지정됨
   console.log(socket.id, "has connected");
   // socket.emit("msgFromServer", { data: "서버: 클라이언트 응답하라" });
   socket.emit("msgFromServer", "서버: 클라이언트 응답하라");
