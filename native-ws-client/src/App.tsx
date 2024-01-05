b1b631983714adfabdf8bb25832cfb72dd8dbc63
jong-k <fully.charged07@gmail.com>
2024-01-05 14:02:05 +0900

    feat: socket 첫 연결

--- native-ws-client/src/App.tsx
+++ native-ws-client/src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import EchoRoom from "./EchoRoom";
+import { socket } from "./socket";
 
 export default function App() {
+  useEffect(() => {
+    socket.onopen = () => {
+      console.log("server connected");
+    };
+  }, []);
+
   return (
     <div>
       <EchoRoom />
---
+++ native-ws-client/src/socket/index.ts
@@ -0,0 +1,3 @@
+const SERVER_URL = "ws://localhost:4004";
+
+export const socket = new WebSocket(SERVER_URL);
